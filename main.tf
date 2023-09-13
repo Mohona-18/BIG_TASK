170dc1e2e7929b3546be97be5848883e6acc23c5
Mohona-18 <80566776+Mohona-18@users.noreply.github.com>
2023-09-13 12:55:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,21 @@ provider "aws" {
     region = "us-east-2"
 	
 }
-
+resource "aws_network_interface" "test" {
+  subnet_id="subnet-0d842abdd2ea9335f"
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
 resource "aws_instance" "ec2-instance" {
     ami = "ami-0ccabb5f82d4c9af5"
     instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0a38294eb1f8c2ac6"]
-	subnet_id="subnet-034db6680b397c28a"
+    network_interface {
+    network_interface_id = aws_network_interface.test.id
+    device_index         = 0
+    }
+
+
 	  key_name      = "win-demo-kp-1"
 	  tags= {
 	  Name= "Linux_Instance_Terraform"
